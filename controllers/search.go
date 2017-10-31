17e0658a54d3735caa4b51e3ad37e0149d85d644
LucaTony <lucaomat@gmail.com>
2017-10-31 14:24:42 +0100

    10-01 Basi UI, post

--- controllers/search.go
+++ controllers/search.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
-	"fmt"
+    "fmt"
     "net/http"
     //"strings"
     //"reflect"
+
+    "github.com/LucaTony/SIMS/models"
 )
 
 var S string
@@ -13,7 +15,7 @@ var S string
 //Testing POST
 func (t *Todo) SearchPost() {
     fmt.Println("SearchPost")
-	req := t.Ctx.Request()
+    req := t.Ctx.Request()
     _ = req.ParseForm()
 
     S = req.PostForm.Get("searchinput")
@@ -25,12 +27,38 @@ func (t *Todo) SearchPost() {
     t.Ctx.Redirect("/search", http.StatusFound)
 }
 
+func remove(s []int, i int) []int {
+    s[len(s)-1], s[i] = s[i], s[len(s)-1]
+    return s[:len(s)-1]
+}
+
 
 //Testing GET
 func (t *Todo) SearchGet() {
     fmt.Println("SearchGet")
     //t.HTML(http.StatusOK)
-	t.Ctx.Data["Data"] = S
-    t.Ctx.Template = "found"
+    todos := []*models.Todo{}
+    //t.Ctx.Data["Data"] = S
+
     //t.Ctx.Redirect("/test", http.StatusFound) //don't redirect
+
+    t.Ctx.DB.Order("created_at desc").Find(&todos)
+
+    for i := range todos {
+        if todos[i].Body != "test" {
+            todos = append(todos[:i], todos[i+1:]...)
+            fmt.Println("deleted!!")
+        }
+    }
+
+    t.Ctx.Data["List"] = todos
+    //fmt.Println(reflect.TypeOf(todos))
+    //for _, v := range todos {fmt.Printf("%v",v.Body)} // Iterate throuh objects, only value (since the _)
+
+
+    //t.Ctx.Template = "index"
+    t.Ctx.Template = "found"
+    t.HTML(http.StatusOK)
+
+
 }
