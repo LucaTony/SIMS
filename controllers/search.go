417f6d9bcc35bf1ac58ea43ee14281e0aaeef09f
mavn1500 <mvangmans@hotmail.com>
2017-10-31 17:30:57 +0100

    Fixes #1

--- controllers/search.go
+++ controllers/search.go
@@ -1,61 +1,59 @@
 package controllers
 
 import (
-    "fmt"
-    "net/http"
-    //"strings"
-    _ "reflect"
-    _ "bytes"
+	"fmt"
+	"net/http"
+	//"strings"
+	_ "bytes"
+	_ "reflect"
 
-    "github.com/LucaTony/SIMS/models"
+	"github.com/LucaTony/SIMS/models"
 )
 
 var mySearch string
 
-
 //Testing POST
 func (t *Todo) SearchPost() {
-    fmt.Println("SearchPost")
-    req := t.Ctx.Request()
-    _ = req.ParseForm()
+	fmt.Println("SearchPost")
+	req := t.Ctx.Request()
+	_ = req.ParseForm()
 
-    mySearch = req.PostForm.Get("searchinput")
-    fmt.Println(mySearch)
+	mySearch = req.PostForm.Get("searchinput")
+	fmt.Println(mySearch)
 
-    //t.HTML(http.StatusOK)
-    //t.Ctx.Template = "/test" //Post doesn't need a template
+	//t.HTML(http.StatusOK)
+	//t.Ctx.Template = "/test" //Post doesn't need a template
 
-    t.Ctx.Redirect("/", http.StatusFound)
+	t.Ctx.Redirect("/", http.StatusFound)
 }
-
 
 //Testing GET
 func (t *Todo) Home() {
-    fmt.Println("SearchGet")
-    //t.HTML(http.StatusOK)
-    todos := []*models.Todo{} // Create empty slice of struct pointers.
-    //t.Ctx.Data["Data"] = S
-    //t.Ctx.Redirect("/test", http.StatusFound) //don't redirect
+	fmt.Println("SearchGet")
+	//t.HTML(http.StatusOK)
+	todos := []*models.Todo{} // Create empty slice of struct pointers.
+	//t.Ctx.Data["Data"] = S
+	//t.Ctx.Redirect("/test", http.StatusFound) //don't redirect
 
-    t.Ctx.DB.Order("created_at desc").Find(&todos)
-    //var t.Ctx.Data["List"]
-    teststring := []string{}
+	t.Ctx.DB.Order("created_at desc").Find(&todos)
+	//var t.Ctx.Data["List"]
+	teststring := []string{}
 
-    for i := range todos {
-        if todos[i].Body == mySearch {
-            teststring = append(teststring, todos[i].Body)
-            fmt.Println("added!!")
-        }
-    }
+	for i := range todos {
+		if todos[i].Body == mySearch {
+			teststring = append(teststring, todos[i].Body)
+			fmt.Println("added!!")
+		}
+	}
 
-    //t.Ctx.Data["Data"] = []string{"This is", "Spartaa"}
-    t.Ctx.Data["Data"] = teststring
+	//t.Ctx.Data["Data"] = []string{"This is", "Spartaa"}
+	t.Ctx.Data["Data"] = teststring
+	mySearch = ""
+	//fmt.Println(reflect.TypeOf(t.Ctx.Data["List"]))
+	//for _, v := range todos {fmt.Printf("%v",v.Body)} // Iterate throuh objects, only value (since the _)
 
-    //fmt.Println(reflect.TypeOf(t.Ctx.Data["List"]))
-    //for _, v := range todos {fmt.Printf("%v",v.Body)} // Iterate throuh objects, only value (since the _)
-
-    t.Ctx.Template = "index"
-    //t.Ctx.Template = "found"
-    t.HTML(http.StatusOK)
+	t.Ctx.Template = "index"
+	//t.Ctx.Template = "found"
+	t.HTML(http.StatusOK)
 
 }
