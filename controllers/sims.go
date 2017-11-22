0ffe46c80cce4d594ae0aefdd5d3285c8a6f2499
LucaTony <lucaomat@gmail.com>
2017-11-22 16:58:48 +0100

    12-00 Cleanup

--- controllers/sims.go
+++ controllers/sims.go
@@ -3,72 +3,35 @@
 package controllers
 
 import (
-    "fmt"
-    "net/http"
-    "strconv"
-
-    "github.com/LucaTony/SIMS/models"
+    //"fmt"
+    //_ "github.com/LucaTony/SIMS/models"
     "github.com/gernest/utron/controller"
     "github.com/gorilla/schema"
 )
 
 var decoder = schema.NewDecoder()
 
-//Todo is a controller for Todo list
-type Todo struct {
+//Search is a controller for a Search list
+type Search struct {
     controller.BaseController
     Routes []string
 }
 
-//Create creates a todo  item
-func (t *Todo) Create() {
-    fmt.Println("Creating..")
-    todo := &models.Todo{}
-    req := t.Ctx.Request()
-    _ = req.ParseForm()
-    if err := decoder.Decode(todo, req.PostForm); err != nil {
-        t.Ctx.Data["Message"] = err.Error()
-        t.Ctx.Template = "error"
-        t.HTML(http.StatusInternalServerError)
-        return
-    }
-
-    t.Ctx.DB.Create(todo)
-    t.Ctx.Redirect("/", http.StatusFound)
-}
-
-//Delete deletes a todo item
-func (t *Todo) Delete() {
-    todoID := t.Ctx.Params["id"]
-    ID, err := strconv.Atoi(todoID)
-    if err != nil {
-        t.Ctx.Data["Message"] = err.Error()
-        t.Ctx.Template = "error"
-        t.HTML(http.StatusInternalServerError)
-        return
-    }
-    t.Ctx.DB.Delete(&models.Todo{ID: ID})
-    t.Ctx.Redirect("/", http.StatusFound)
-}
-
-//Testing is a test function
-func (t *Todo) Testing() {
-    fmt.Println("Testing")
-}
-
-
-//NewTodo returns a new  todo list controller
-func NewTodo() controller.Controller {
-    return &Todo{
+//NewSims returns a new sims list controller
+func NewSims() controller.Controller {
+    return &Search{
         Routes: []string{
             "get;/;Home",
 
-            "post;/search;SearchPost",
-            "get;/search;SearchGet",
+            "post;/search;HousePost",
+            "get;/search;HouseGet",
 
             "post;/create;Create",
             "get;/delete/{id};Delete",
-            "get;/update;UpdateGet",
+            "get;/scraper;ScraperGet",
+            "post;/scraper;ScraperPost",
+            "get;/calculator;CalcGet",
+            "post;/calculator;CalcPost",
         },
     }
 }
