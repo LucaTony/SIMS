61e9a67d9d3de55f3f70990a562a830f5aaf0b94
LucaTony <lucaomat@gmail.com>
2017-12-02 16:39:51 +0100

    07-00 Add Eco-Calculator, present results

--- controllers/calculator.go
+++ controllers/calculator.go
@@ -1,49 +1,135 @@
 package controllers
 
 import (
-	"fmt"
-
-	"github.com/LucaTony/SIMS/models"
+    "fmt"
+    "net/http"
+    "github.com/LucaTony/SIMS/models"
+    "github.com/gernest/utron/controller"
+    "reflect"
+    "strconv"
+    "strings"
 )
 
-type Quiz struct {
-	Question string
-	Option1  string
-	Option2  string
-	Option3  string
-	Option4  string
+type CalcSend struct {
+    ID          int
+    Question    string
+    Option01    string
+    Option02    string
+    Option03    string
+    Option04    string
+    Option05    string
+    Option06    string
+}
+
+//Calc is a controller for a Calculator list
+type Calc struct {
+    controller.BaseController
+    Routes []string
+}
+//Result is a controller for a Calculator list
+type Result struct {
+    controller.BaseController
+    Routes []string
+}
+
+var myScore float32             //Score in percentage
+var myScoreSet bool = false     //True if test was taken
+var myRecomm = []string{}       //Show recomm on startpage
+var myResultID int              //id, primary key for db for unique link
+var myResultRecomm string       //Store Recomm for for future
+
+func getField(c *models.Calculator, field string) float32 {
+    r := reflect.ValueOf(c)
+    f := reflect.Indirect(r).FieldByName(field)
+    return float32(f.Float())
 }
 
 //CalcPost function to get questions and options from db
 // and then display it on the website.
-func (S *Search) CalcPost() {
-	fmt.Println("CalcPost")
-	quiz := []*models.Calculator{}
+func (c *Calc) CalcPost() {
+    fmt.Println("CalcPost")
+    req := c.Ctx.Request()
+    _ = req.ParseForm()
+    //fmt.Println(req.PostForm)
 
-	//container type struct, struct Quiz from top of file
-	var quest = []Quiz{}
+    questions := []*models.Calculator{}
+    c.Ctx.DB.Order("id").Find(&questions)
 
-	var option = []Quiz{}
+    myScore = 0
+    myIndex := 0        //usually id-1
+    recommInd := 0      //Index for recomm values
+    //TODO: Refactor DBlookup
+    // rq: Question1, o: [2]
+    //Iteration order is like in db i.e. id
+    for q := range questions {
+        tempScore := getField(questions[q], "Points0" + req.PostForm["Question"+ strconv.Itoa(myIndex+1)][0] )
+        if (tempScore == 0) {
+            myRecomm = append(myRecomm, questions[q].Recomm)
+            recommInd++
+            myResultRecomm += strconv.Itoa(myIndex+1)
+            myResultRecomm += (" ") //Delimiter hack
+        }
+        myScore += tempScore
+        myIndex++
+    }
 
-	//declaration and initilalization of variables
-	question := Quiz{}
-	options := Quiz{}
+    myScore *= 10
+    myScoreSet = true
+    fmt.Println("Your score is : ", myScore)
 
-	for i := range quiz {
-		//Get the question from the db.
-		question = Quiz{
-			Question: quiz[i].Question,
-		}
-		for j := range quiz {
-			//Get the responding answer options to each question.
-			options = Quiz{
-				Option1: quiz[j].Option01,
-				Option2: quiz[j].Option02,
-				Option3: quiz[j].Option03,
-				Option4: quiz[j].Option04,
-			}
-		}
-	}
-	quest = append(quest, question)
-	option = append(option, options)
+    c.ResultSave()
+    c.Ctx.Redirect("/#calculator", http.StatusFound)
+}
+
+func (c *Calc) ResultSave() {
+    result := &models.Result{
+        Total: myScore,
+        Recomm: myResultRecomm,
+    }
+    c.Ctx.DB.Create(result)
+    myResultID = result.ID
+    c.Ctx.Data["ResultLinkSend"] = myResultID
+    fmt.Println("Your ID for the result is: ", result.ID)
+}
+
+func (c *Calc) ResultGet() {
+    resultID := c.Ctx.Params["id"]
+    ID, _ := strconv.Atoi(resultID) // int not needed
+    //myFound := c.Ctx.DB.Select("total").Find(&models.Result{ID: ID})
+    results := []*models.Result{}
+    c.Ctx.DB.First(&results, ID)
+    //fmt.Println(results[0])
+    c.Ctx.Data["ResultSend"] = results[0]
+    myTempRecommId := strings.Fields(results[0].Recomm) //get recomm ids
+    myTempRecomm := []string{}
+
+    calculators := []*models.Calculator{}
+    for i := range myTempRecommId {
+        tempid, err := strconv.Atoi(myTempRecommId[i])
+        if err != nil {
+            fmt.Println("RecommId couldn't be Atoi")
+        }
+        c.Ctx.DB.First(&calculators, tempid)
+        myTempRecomm = append(myTempRecomm, calculators[0].Recomm)
+        fmt.Println(calculators[0].Recomm)
+    }
+
+    c.Ctx.Data["SavedRecomm"] = myTempRecomm
+
+    c.Ctx.Template = "result"
+    c.HTML(http.StatusOK)
+}
+
+
+//NewCalc returns a new sims list controller
+func NewCalc() controller.Controller {
+    return &Calc{
+        Routes: []string{
+            "get;/calculator;CalcGet",
+            "post;/calculator;CalcPost",
+            "get;/save;ResultSave",
+            "get;/result/{id};ResultGet",
+            //"get;/;CalcPost",
+        },
+    }
 }
--- controllers/search.go
+++ controllers/search.go
@@ -8,25 +8,31 @@ import (
     _ "reflect"
     "sort"
     "github.com/LucaTony/SIMS/models"
+    "github.com/gernest/utron/controller"
 )
 
 var mySearch string // Text from the user search input field
 
-// Datatype for the Data to send to the HTML
-type DataSend struct {
+// SearchSend for the Data to send to the HTML
+type SearchSend struct {
     Title string
     Body  string
     Url   string
     Score int
 }
 
-type ByScore []DataSend
+//Search is a controller for a Search list
+type Search struct {
+    controller.BaseController
+    Routes []string
+}
 
-// Functions for sorting the Search results by DataSend.Score
+type ByScore []SearchSend
+
+// Functions for sorting the Search results by SearchSend.Score
 func (a ByScore) Len() int           { return len(a) }
 func (a ByScore) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByScore) Less(i, j int) bool { return a[i].Score > a[j].Score }
-
 
 //Search gets the data from the searchinput field
 func (t *Search) HousePost() {
@@ -44,13 +50,13 @@ func (t *Search) HousePost() {
 
 //Home is the root get that shows the webpage and the search results if the search function is used
 //BUG(r): The searchinput should remain in the field when reloading
-func (t *Search) Home() {
-    fmt.Println("SearchGet")
+func (t *Search)Home() {
+    fmt.Println("Home")
     searches := []*models.Search{} // Create empty slice of struct pointers.
 
     t.Ctx.DB.Order("created_at desc").Find(&searches)
 
-    var mySend = []DataSend {}
+    var mySend = []SearchSend {}
 
     if (mySearch != ""){
         words := strings.Fields(mySearch)
@@ -63,7 +69,7 @@ func (t *Search) Home() {
                 }
             }
             if (score > 0) {
-                tempSend := DataSend{ //TODO: Direct
+                tempSend := SearchSend{ //TODO: Direct
                     Title: searches[i].Title,
                     Body:  searches[i].Body,
                     Url:   searches[i].URL,
@@ -75,15 +81,70 @@ func (t *Search) Home() {
         }
     }
 
-    //t.Ctx.Data["Data"] = []string{"This is", "Spartaa"}
     sort.Sort(ByScore(mySend))
     t.Ctx.Data["Data"] = mySend
     mySearch = "" // TODO: don't remove the searchfield text
-
-    //fmt.Println(reflect.TypeOf(t.Ctx.Data["List"]))
     //for _, v := range searches {fmt.Printf("%v",v.Body)} // Iterate throuh objects, only value (since the _)
 
+    //cal := &Calc{}
+    //var cal *Calc
+    //cal.CalcGet()
+    t.CalcGet()
+    t.FactGet()
+
     t.Ctx.Template = "index"
-    //t.Ctx.Template = "testing"
     t.HTML(http.StatusOK)
+}
+
+
+//TODO: Refactor and understand
+func (c Search) CalcGet(){
+    questions := []*models.Calculator{}
+    c.Ctx.DB.Order("id").Find(&questions)
+    //fmt.Println(questions)
+    mySend := []CalcSend {}
+
+    //Get the question from the db.
+    //Get the responding answer options to each question.
+    for q := range questions {
+        tempSend := CalcSend{
+            ID:         questions[q].ID,
+            Question:   questions[q].Question,
+            Option01:   questions[q].Option01,
+            Option02:   questions[q].Option02,
+            Option03:   questions[q].Option03,
+            Option04:   questions[q].Option04,
+            Option05:   questions[q].Option05,
+            Option06:   questions[q].Option06,
+        }
+        mySend = append(mySend, tempSend)
+        //fmt.Println(mySend)
+    }
+    c.Ctx.Data["CalcSend"] = mySend
+    //c.Ctx.Data["add"] = add(3,4)
+    if (myScoreSet) {
+        c.Ctx.Data["CalcResult"] = myScore
+        c.Ctx.Data["CalcRecomm"] = myRecomm
+        c.Ctx.Data["ResultLinkSend"] = myResultID
+    }
+
+    myScoreSet = false //Flush
+    myRecomm = myRecomm[:0]
+
+}
+func (c Search) FactGet() {
+    facts := []*models.FunFact{}
+    c.Ctx.DB.Order("id").Find(&facts)
+    c.Ctx.Data["Fact"] = facts
+
+}
+
+//NewSearch returns a new search list controller
+func NewSearch() controller.Controller {
+    return &Search{
+        Routes: []string{
+            "get;/;Home",
+            "post;/search;HousePost",
+        },
+    }
 }
