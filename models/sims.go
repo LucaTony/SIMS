a29459e8fd3a47cb30efb11c0dd1ae911a895392
LucaTony <lucaomat@gmail.com>
2017-12-07 14:41:30 +0100

    07-00 Add Accordion with suggestions

--- models/sims.go
+++ models/sims.go
@@ -28,7 +28,12 @@ type Calculator struct {
     Type       string      `schema:"type"`
     Amount     int         `schema: "amount"`
     Question   string      `schema:"quest"`
-    Recomm     string       `schema:"-"`
+    Recomm     string     `schema:"-"`
+    Recomm1     string     `schema:"-"`
+    Recomm2     string     `schema:"-"`
+    Recomm3     string     `schema:"-"`
+    Recomm4     string     `schema:"-"`
+    Recomm5     string     `schema:"-"`
     Option01    string      `schema:"-"`
     Points01    float32     `schema:"-"`
     Option02    string      `schema:"-"`
