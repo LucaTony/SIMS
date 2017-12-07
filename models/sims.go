551db87b64a1ca0af077edbc28d6e9383c37d061
mavn1500 <mvangmans@hotmail.com>
2017-12-07 17:12:14 +0100

    02-05 Add url

--- models/sims.go
+++ models/sims.go
@@ -1,68 +1,70 @@
 package models
 
 import (
-    "time"
+	"time"
 )
 
 //Person represent an item of search list
 type Search struct {
-    ID          int         `schema:"-"`
-    Title       string      `schema:"title"`
-    Body        string      `schema:"body"`
-    URL	        string      `schema:"url"`
-    Dom         string      `schema:"dom"`
-    DomTitle    string      `schema:"-"`
-    UpdatedAt   time.Time   `schema:"-"`
-    CreatedAt   time.Time   `schema:"-"`
+	ID        int       `schema:"-"`
+	Title     string    `schema:"title"`
+	Body      string    `schema:"body"`
+	URL       string    `schema:"url"`
+	Dom       string    `schema:"dom"`
+	DomTitle  string    `schema:"-"`
+	UpdatedAt time.Time `schema:"-"`
+	CreatedAt time.Time `schema:"-"`
 }
 
 //Test represent an a test in a test list
 type Test struct {
-    ID         int         `schema:"-"`
-    Expected   string      `schema:"exp"`
+	ID       int    `schema:"-"`
+	Expected string `schema:"exp"`
 }
 
 //Calculator represent the questions for the calculator
 type Calculator struct {
-    ID         int         `schema:"-"`
-    Type       string      `schema:"type"`
-    Amount     int         `schema: "amount"`
-    Question   string      `schema:"quest"`
-    Recomm     string       `schema:"-"`
-    Option01    string      `schema:"-"`
-    Points01    float32     `schema:"-"`
-    Option02    string      `schema:"-"`
-    Points02    float32     `schema:"-"`
-    Option03    string      `schema:"-"`
-    Points03    float32     `schema:"-"`
-    Option04    string      `schema:"-"`
-    Points04    float32     `schema:"-"`
-    Option05    string      `schema:"-"`
-    Points05    float32     `schema:"-"`
-    Option06    string      `schema:"-"`
-    Points06    float32     `schema:"-"`
-    Option07    string      `schema:"-"`
-    Points07    float32     `schema:"-"`
-    Option08    string      `schema:"-"`
-    Points08    float32     `schema:"-"`
-    Option09    string      `schema:"-"`
-    Points09    float32     `schema:"-"`
-    Option10    string      `schema:"-"`
-    Points10    float32     `schema:"-"`
+	ID       int     `schema:"-"`
+	Type     string  `schema:"type"`
+	Amount   int     `schema: "amount"`
+	Question string  `schema:"quest"`
+	Recomm   string  `schema:"-"`
+	Option01 string  `schema:"-"`
+	Points01 float32 `schema:"-"`
+	Option02 string  `schema:"-"`
+	Points02 float32 `schema:"-"`
+	Option03 string  `schema:"-"`
+	Points03 float32 `schema:"-"`
+	Option04 string  `schema:"-"`
+	Points04 float32 `schema:"-"`
+	Option05 string  `schema:"-"`
+	Points05 float32 `schema:"-"`
+	Option06 string  `schema:"-"`
+	Points06 float32 `schema:"-"`
+	Option07 string  `schema:"-"`
+	Points07 float32 `schema:"-"`
+	Option08 string  `schema:"-"`
+	Points08 float32 `schema:"-"`
+	Option09 string  `schema:"-"`
+	Points09 float32 `schema:"-"`
+	Option10 string  `schema:"-"`
+	Points10 float32 `schema:"-"`
 }
+
 //Result represent the results from the calculator
 type Result struct {
-    ID         int         `schema:"-"`
-    Total      float32         `schema:"total"`
-    CreatedAt   time.Time   `schema:"-"`
-    Recomm     string      `schema:"-"`
+	ID        int       `schema:"-"`
+	Total     float32   `schema:"total"`
+	CreatedAt time.Time `schema:"-"`
+	Recomm    string    `schema:"-"`
 }
 
 //FunFact represent the results from the calculator
 type FunFact struct {
-    ID          int         `schema:"-"`
-    Category    string      `schema:"-"`
-    Fact        string      `schema:"-"`
+	ID       int    `schema:"-"`
+	Category string `schema:"-"`
+	Fact     string `schema:"-"`
+	URL      string `schema:"-"`
 }
 
 //TableName is a function to modify the standard name
@@ -70,4 +72,3 @@ type FunFact struct {
 //func (*Results) TableName() {
 //return "calculator_res"
 //}
-
